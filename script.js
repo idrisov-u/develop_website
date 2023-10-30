8e6236f94b78d45cd9dc8b18a71204c2ff947b82
Umidzhon Idrisov <idrisov_u@auca.kg>
2023-10-31 05:00:20 +0600

    second commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    
+    alert('Спасибо за ваш интерес! Мы свяжемся с вами в ближайшее время.');
+    this.reset(); // Clear the form fields
+});
